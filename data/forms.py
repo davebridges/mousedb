2cdc5d36d235a1a614c370404115ef4d7c07286f
davebridges <dave.bridges@gmail.com>
2010-02-14 12:21:44 -0500

    Added generic views, urlconfs and templates for Treatment objects

--- data/forms.py
+++ data/forms.py
@@ -1,9 +1,9 @@
-from mousedb.data.models import Experiment, Measurement, Study
-from mousedb.animal.models import Animal
 from django.forms import ModelForm
 from django import forms
 from django.contrib.admin import widgets
 
+from mousedb.data.models import Experiment, Measurement, Study, Treatment
+from mousedb.animal.models import Animal
 
 class ExperimentForm(ModelForm):
 	animals = forms.ModelMultipleChoiceField(queryset=Animal.objects.all(), widget=widgets.FilteredSelectMultiple("animals",True))
@@ -25,3 +25,9 @@ class StudyForm(ModelForm):
 	class Meta:
 		model = Study
 
+class TreatmentForm(ModelForm):
+	"""Form class for study treatment groups.
+
+	In the case of studies, animals are defined in the treatment group rather than in the study group.  A treatment consists of a study, a set of animals and the conditions which define that treatment.  This includes related fields for environment, diet, implants and transplants."""
+	class Meta:
+		model = Treatment
