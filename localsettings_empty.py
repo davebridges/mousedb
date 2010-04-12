c8ba4d6a059045e6bb64b23157153fe4ce49fb39
davebridges <dave.bridges@gmail.com>
2010-04-11 20:29:01 -0400

    updated localsettings_empty with new database settings

--- localsettings_empty.py
+++ localsettings_empty.py
@@ -1,6 +1,11 @@
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'NAME': '', # Or path to database file if using sqlite3.
+        'ENGINE': '', #  Choose one of 'django.db.backends.postgresql_psycopg2','django.db.backends.postgresql', 'django.db.backends.mysql', 'django.db.backends.sqlite3', 'django.db.backends.oracle'
+        'USER': '',  # Not used with sqlite3.
+        'PASSWORD': '', # Not used with sqlite3
+	'HOST':, # Set to empty string for localhost. Not used with sqlite3.
+	'PORT':, # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
