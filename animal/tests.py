b724b5101607b0376216c9bb72f64925c389862e
davebridges <dave.bridges@gmail.com>
2010-05-15 12:20:00 -0400

    Over-rode save for breeding cages, to update the cage for the male and female animals.  Created several tests to validate this logic

--- animal/tests.py
+++ animal/tests.py
@@ -69,3 +69,40 @@ class BreedingModelTests(TestCase):
         test_breeding.save()
         self.assertEquals(test_breeding.Active, False)
 
+    def test_update_breeding_cage_automatically_move_animal(self):
+        """This is a test for creating a new breeding object, with only the minimum being entered.  It also tests whether the cages are updated to the breeding cage when that attribute is set.  This tests if only one female is in a breeding set."""
+        example_strain = Strain(Strain="Example Strain")
+        example_strain.save()
+        example_male = Animal(Strain = example_strain, Genotype="-/-", Background="Mixed", Gender='M', Cage=1111)
+        example_male.save()
+        self.assertEquals(example_male.Cage, 1111)
+        example_female = Animal(Strain = example_strain, Genotype="-/-", Background="Mixed", Gender='F', Cage=2222)
+        example_female.save()
+        self.assertEquals(example_female.Cage, 2222)
+        new_breeding = Breeding(Strain = example_strain, Cage=3333)
+        new_breeding.male = example_male
+        new_breeding.females = example_female
+        new_breeding.save()
+        self.assertEquals(example_male.Cage, 3333)
+        self.assertEquals(example_female.Cage, 3333)
+	
+    def test_update_breeding_cage_automatically_move_animal_two_females(self):
+        """This is a test for creating a new breeding object, with only the minimum being entered.  It also tests whether the cages are updated to the breeding cage when that attribute is set.  This tests if more than one female is in a breeding set."""
+        example_strain = Strain(Strain="Example Strain")
+        example_strain.save()
+        example_male = Animal(Strain = example_strain, Genotype="-/-", Background="Mixed", Gender='M', Cage=1111)
+        example_male.save()
+        self.assertEquals(example_male.Cage, 1111)
+        example_female1 = Animal(Strain = example_strain, Genotype="-/-", Background="Mixed", Gender='F', Cage=2222)
+        example_female1.save()
+        example_female2 = Animal(Strain = example_strain, Genotype="-/-", Background="Mixed", Gender='F', Cage=3333)
+        example_female2.save()
+        self.assertEquals(example_female1.Cage, 2222)
+        self.assertEquals(example_female2.Cage, 3333)
+        new_breeding = Breeding(Strain = example_strain, Cage=4444)
+        new_breeding.male = example_male
+        new_breeding.females = example_female1, example_female2
+        new_breeding.save()
+        self.assertEquals(example_male.Cage, 4444)
+        self.assertEquals(example_female1.Cage, 4444)
+        self.assertEquals(example_female2.Cage, 4444)
