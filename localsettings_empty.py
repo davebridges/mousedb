7aa0d61fa0408f244aade74eaabb5df7938f3407
Dave Bridges <dave.bridges@gmail.com>
2010-05-20 15:12:47 -0400

    Fixed typo in localsettings_empty.py

--- localsettings_empty.py
+++ localsettings_empty.py
@@ -20,8 +20,8 @@ DATABASES = {
         'ENGINE': '', #  Choose one of 'django.db.backends.postgresql_psycopg2','django.db.backends.postgresql', 'django.db.backends.mysql', 'django.db.backends.sqlite3', 'django.db.backends.oracle'
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '', # Not used with sqlite3
-	'HOST':, # Set to empty string for localhost. Not used with sqlite3.
-	'PORT':, # Set to empty string for default. Not used with sqlite3.
+	'HOST':'', # Set to empty string for localhost. Not used with sqlite3.
+	'PORT':'', # Set to empty string for default. Not used with sqlite3.
     }
 }
 
