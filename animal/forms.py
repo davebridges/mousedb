4d44b5bb16e5f2f84303e1d4a6d17e76cfb4aed4
Dave Bridges <davebridges@gmail.com>
2010-06-14 21:03:20 -0400

    Added autocomplete widgets for breeding_form

--- animal/forms.py
+++ animal/forms.py
@@ -11,7 +11,7 @@ class AnimalForm(ModelForm):
 	"""This modelform provides fields for modifying animal data.
 	
 	It includes all fields except CageID (will be deprecated) and Alive (which is automattically set upon saving the animal).
-	This form also automatically loads javascript and css for the datepicker jquery-ui widget."""
+	This form also automatically loads javascript and css for the datepicker jquery-ui widget.  It also includes auto"""
 	Father = AutoCompleteSelectField('animal', required=False)
 	Mother = AutoCompleteSelectField('animal', required=False)
 	class Meta:
@@ -27,11 +27,18 @@ class AnimalForm(ModelForm):
 class BreedingForm(ModelForm):
     """This form provides most fields for creating and entring breeding cage data.
 	
-    This form is used from the url /mousedb/breeding/new and is a generic create view.  The only excluded field is CageID, as this feature will be deprecated.
+    This form is used from the url /mousedb/breeding/new and is a generic create view.  The only excluded field is CageID, as this feature will be deprecated.  This view includes a datepicker widget for Stat and End dates and autocomplete fields for the Females and Male fields
     """
+    Male = AutoCompleteSelectMultipleField('animal', required=False)
+    Females = AutoCompleteSelectMultipleField('animal', required=False)
     class Meta:
         model = Breeding
         exclude = ['CageID', 'Active']
+    class Media:
+		css = {
+			'all': ('javascript/jquery-ui/css/custom-theme/jquery-ui-1.7.2.custom.css','javascript/jquery-autocomplete/jquery.autocomplete.css', 'css/iconic.css')
+				}
+		js = ('javascript/jquery-1.3.2.js','javascript/jquery-ui/js/jquery-ui-1.7.2.custom.min.js', 'javascript/jquery-autocomplete/jquery.autocomplete.js')
 
 
 
