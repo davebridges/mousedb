8da8a4d491a7a2e9640bae39e9c62e0bb6fff3f9
Dave Bridges <dave.bridges@gmail.com>
2010-11-02 12:16:20 -0400

    Updated plug event displays from breeding detail

--- timed_mating/views.py
+++ timed_mating/views.py
@@ -12,21 +12,21 @@ from mousedb.timed_mating.forms import BreedingPlugForm
 
 @permission_required('timed_mating.add_plugevents')
 def breeding_plugevent(request, breeding_id):
-	"""This view defines a form for adding new plug events from a breeding cage.
+    """This view defines a form for adding new plug events from a breeding cage.
 
-        This form requires a breeding_id from a breeding set and restricts the PlugFemale and PlugMale to animals that are defined in that breeding cage."""
-	breeding = Breeding.objects.select_related().get(id=breeding_id)
-        if request.method == "POST":
-		form = BreedingPlugForm(request.POST, request.FILES)
-                if form.is_valid():
-                        plug = form.save(commit=False)
-                        plug.Breeding_id = breeding.id
-                        plug.save()
-                        form.save()
-                        return HttpResponseRedirect("/mousedb/plug/")
-        else:
-                form = BreedingPlugForm()
-		form.fields["PlugFemale"].queryset = breeding.Females.all()
-		form.fields["PlugMale"].queryset = breeding.Male.all()
-        return render_to_response('breedingplug_form.html', {'form':form, 'breeding':breeding},context_instance=RequestContext(request))
+    This form requires a breeding_id from a breeding set and restricts the PlugFemale and PlugMale to animals that are defined in that breeding cage."""
+    breeding = Breeding.objects.select_related().get(id=breeding_id)
+    if request.method == "POST":
+        form = BreedingPlugForm(request.POST, request.FILES)
+        if form.is_valid():
+            plug = form.save(commit=False)
+            plug.Breeding_id = breeding.id
+            plug.save()
+            form.save()
+            return HttpResponseRedirect("/mousedb/plug/")
+    else:
+        form = BreedingPlugForm()
+        form.fields["PlugFemale"].queryset = breeding.Females.all()
+        form.fields["PlugMale"].queryset = breeding.Male.all()
+    return render_to_response('breedingplug_form.html', {'form':form, 'breeding':breeding},context_instance=RequestContext(request))
 
