49ef283ee98ddf642f2f2fd81b25cfccce4af23b
davebridges <dave.bridges@gmail.com>
2011-03-26 12:04:48 -0400

    added staticfiles context processor to settings.py

--- src/mousedb/settings.py
+++ src/mousedb/settings.py
@@ -53,6 +53,7 @@ TEMPLATE_CONTEXT_PROCESSORS =(
 	"django.core.context_processors.debug",
 	"django.core.context_processors.i18n",
 	"django.core.context_processors.media",
+    "django.core.context_processors.static",
 	'django.contrib.messages.context_processors.messages',
 	"mousedb.context_processors.group_info",
 )
