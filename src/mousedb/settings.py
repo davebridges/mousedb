4ea6ffa7f416ff6ddd1a75d654ec7bb8b6573363
Dave Bridges <dave.bridges@gmail.com>
2011-03-29 16:18:12 -0400

    Changed admin media prefix

--- src/mousedb/settings.py
+++ src/mousedb/settings.py
@@ -22,7 +22,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/mousedb-media/admin-media/'
+ADMIN_MEDIA_PREFIX = '/static/admin-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'ci%^08ig-0qu*&b(kz_=n6lvbx*puyx6=8!yxzm0+*z)w@7+%6'
