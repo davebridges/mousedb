440131b5360b5972361266b1f3e610aaaa6d8b2c
davebridges <dave.bridges@gmail.com>
2011-04-09 20:57:15 -0400

    factored out views from urls.py  Tested this with dev server and testing framework and it seems ok

--- src/mousedb/timed_mating/urls.py
+++ src/mousedb/timed_mating/urls.py
@@ -4,14 +4,14 @@ It takes a url in the form of /plug/something and sends it to the appropriate vi
 
 from django.conf.urls.defaults import *
 
-from mousedb.timed_mating.views import PlugEventsList, StrainPlugEventsList, PlugEventsDetail, PlugEventsCreate, PlugEventsUpdate, PlugEventsDelete
+from mousedb.timed_mating import views
 
 urlpatterns = patterns('',
-    url(r'^$', PlugEventsList.as_view(), name="plugevents-list"),
-    url(r'^strain/(\w*)/$', StrainPlugEventsList.as_view(), name="strain-plugevents-list"),
-    url(r'^(?P<pk>\d*)/$', PlugEventsDetail.as_view(), name="plugevents-detail"),
-    url(r'^new/$', PlugEventsCreate.as_view(), name="plugevents-new"),
-    url(r'^(?P<pk>\d*)/edit/$', PlugEventsUpdate.as_view(), name="plugevents-edit"),
-    url(r'^(?P<pk>\d*)/delete/$', PlugEventsDelete.as_view(), name="plugevents-delete"),
+    url(r'^$', views.PlugEventsList.as_view(), name="plugevents-list"),
+    url(r'^strain/(\w*)/$', views.StrainPlugEventsList.as_view(), name="strain-plugevents-list"),
+    url(r'^(?P<pk>\d*)/$', views.PlugEventsDetail.as_view(), name="plugevents-detail"),
+    url(r'^new/$', views.PlugEventsCreate.as_view(), name="plugevents-new"),
+    url(r'^(?P<pk>\d*)/edit/$', views.PlugEventsUpdate.as_view(), name="plugevents-edit"),
+    url(r'^(?P<pk>\d*)/delete/$', views.PlugEventsDelete.as_view(), name="plugevents-delete"),
     url(r'^breeding/(?P<breeding_id>\d*)/new', 'mousedb.timed_mating.views.breeding_plugevent', name="breeding-plugevents-new"),
 )
