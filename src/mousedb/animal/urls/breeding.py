31024c8a398e430a9b3bb28250b0cff4db36b597
davebridges <dave.bridges@gmail.com>
2011-06-18 09:52:40 -0400

    Removed un-necessary imports from breeding.py

--- src/mousedb/animal/urls/breeding.py
+++ src/mousedb/animal/urls/breeding.py
@@ -3,29 +3,8 @@
 This includes generic views for breeding, breeding details and create, change and delete breeding cages."""
 
 from django.conf.urls.defaults import *
-from django.views.generic.list_detail import object_list
-from django.views.generic.create_update import create_object, update_object, delete_object
-from django.contrib.auth.decorators import login_required, permission_required
 
 from mousedb.animal import views
-from mousedb.animal.models import Breeding
-from mousedb.animal.forms import BreedingForm
-
-@login_required
-def limited_object_list(*args, **kwargs):
-	return object_list(*args, **kwargs)
-
-@permission_required('animal.add_breeding')
-def create_breeding(*args, **kwargs):
-	return create_object(*args, **kwargs)
-
-@permission_required('animal.change_breeding')
-def change_breeding(*args, **kwargs):
-	return update_object(*args, **kwargs)
-
-@permission_required('animal.delete_breeding')
-def delete_breeding(*args, **kwargs):
-	return delete_object(*args, **kwargs)
 
 urlpatterns = patterns('',
 	url(r'^$', views.BreedingList.as_view(), name="breeding-list"),
@@ -38,5 +17,6 @@ urlpatterns = patterns('',
 	url(r'^(?P<breeding_id>\d*)/multiple/?$', 'mousedb.animal.views.multiple_breeding_pups', name="animal-multiple-pups-new"),	
 	url(r'^new/?$', views.BreedingCreate.as_view(), name="breeding-new"),
 	url(r'^(?P<pk>\d*)/edit/?$', views.BreedingUpdate.as_view(), name="breeding-edit"),
+	url(r'^(?P<pk>\d*)/update/?$', views.BreedingUpdate.as_view(), name="breeding-edit"),    
 	url(r'^(?P<pk>\d*)/delete/?$', views.BreedingDelete.as_view(), name="breeding-delete"),
 )
