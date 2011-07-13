adfef7ce8af91725701daf7d148f79fc09203168
Dave Bridges <dave.bridges@gmail.com>
2011-07-13 10:45:53 -0400

    added LogEntry admin view to groups/admin.py

--- src/mousedb/groups/admin.py
+++ src/mousedb/groups/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+
 from mousedb.groups.models import License, Group
+
 
 class GroupAdmin(admin.ModelAdmin):
     """Defines the admin interface for Groups.
@@ -14,3 +17,9 @@ class LicenseAdmin(admin.ModelAdmin):
     Currently set as default."""
     pass
 admin.site.register(License, LicenseAdmin)
+
+class LogEntryAdmin(admin.ModelAdmin):
+    """Defines the admin interface for the LogEntry objects."""
+    
+    list_display = ('user', 'content_type', 'object_id')
+admin.site.register(LogEntry, LogEntryAdmin)
