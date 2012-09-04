51222c8d77b7388b77cd31668bdd8e4bcda09532
Dave Bridges <dave.bridges@gmail.com>
2012-09-04 14:29:03 -0400

    Added STATIC_FILES_DIRS and removed ADMIN_MEDIA_PREFIX from settings.py  Still need to move MEDIA_ROOT and STATIC_ROOT to localsettings and update installation instructions.

--- mousedb/settings.py
+++ mousedb/settings.py
@@ -18,11 +18,7 @@ USE_I18N = True
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(PROJECT_DIR, "media")
 STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/static/admin-media/'
+STATIC_FILES_DIRS = (os.path.join(PROJECT_DIR, "static"),)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'ci%^08ig-0qu*&b(kz_=n6lvbx*puyx6=8!yxzm0+*z)w@7+%6'
