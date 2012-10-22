776f62c31d1bcd78416e244bf529c66389117652
Dave Bridges <dave.bridges@gmail.com>
2012-10-22 10:23:54 -0400

    Removed autocomplete field from TreatmentForm.  Part of issue #32.

--- mousedb/data/forms.py
+++ mousedb/data/forms.py
@@ -52,7 +52,7 @@ class TreatmentForm(ModelForm):
     """Form class for study treatment groups.
 
     In the case of studies, animals are defined in the treatment group rather than in the study group.  A treatment consists of a study, a set of animals and the conditions which define that treatment.  This includes related fields for environment, diet, implants and transplants."""
-    animals = AutoCompleteSelectMultipleField('animal')
+    #animals = AutoCompleteSelectMultipleField('animal')
     class Meta:
         model = Treatment
     class Media:
