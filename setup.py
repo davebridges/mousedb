d58583d23e9e74e5610764e96c86d3bab4ae5049
Dave Bridges <dave.bridges@gmail.com>
2012-11-19 18:29:22 -0500

    Added setup.py file for use with a virtualenv.  Part of issue #42.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='mousedb',
+      version='1.1.1',
+      description="MouseDB is a data management and analysis system for experimental animals",
+      long_description=open('README.rst').read(),
+      author='Dave Bridges',
+      author_email='dave.bridges@gmail.com',
+      license='BSD 2-Clause License',
+      packages=['mousedb'],
+      zip_safe=False,
+      install_requires=[
+          'Django',
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
