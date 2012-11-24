f4eb827c4930f9fd6852b6ae2957de4da93a532c
Dave Bridges <dave.bridges@gmail.com>
2012-11-23 23:15:42 -0500

    Fixed manual imports, part of issue #42.

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../mousedb/gro
 from django.core.management import setup_environ
 import mousedb.settings
 
-setup_environ(settings)
+setup_environ(mousedb.settings)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mousedb.settings'
 
