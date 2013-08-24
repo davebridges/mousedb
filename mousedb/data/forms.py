e01d9a5cac7d8eec27b844b30483003e3120fb9c
Dave Bridges <dave.bridges@gmail.com>
2013-08-24 12:55:58 -0500

    Repaired a merge error in data/forms.py

--- mousedb/data/forms.py
+++ mousedb/data/forms.py
@@ -51,7 +51,6 @@ class StudyForm(ModelForm):
 class TreatmentForm(ModelForm):
     """Form class for study treatment groups.
 
-<<<<<<< HEAD
     In the case of studies, animals are defined in the treatment group rather than in the study group.  
     A treatment consists of a study, a set of animals and the conditions which define that treatment.  
     This includes related fields for environment, diet, implants and transplants."""
@@ -70,10 +69,3 @@ class CohortForm(ModelForm):
         queryset=Animal.objects.filter(Alive=True))
     class Meta:
         model = Cohort
-=======
-    In the case of studies, animals are defined in the treatment group rather than in the study group.  A treatment consists of a study, a set of animals and the conditions which define that treatment.  This includes related fields for environment, diet, implants and transplants."""
-    animals = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-        queryset=Animal.objects.all())
-    class Meta:
-        model = Treatment
->>>>>>> b08f77528cf3648df7c5a4cccae0ef4c4c908b5d
