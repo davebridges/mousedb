893bed743a05a75cbd5345178ccf385404536bb6
Dave Bridges <dave.bridges@gmail.com>
2016-05-31 09:15:41 -0400

    Upgraded to new wsgi format, part of issue #14

--- mousedb/wsgi.py
+++ mousedb/wsgi.py
@@ -1,9 +1,2 @@
-import os
-import sys
-
-sys.path.append('/var/www/internal/mousedb')
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mousedb.settings")
