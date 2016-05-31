11170cbd9c920a24700b4e4252f6541d448f98df
Dave Bridges <dave.bridges@gmail.com>
2016-05-31 14:10:04 -0400

    Added templates directory, part of #14

--- mousedb/settings.py
+++ mousedb/settings.py
@@ -40,15 +40,28 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, "static"),
 )
 
-TEMPLATE_CONTEXT_PROCESSORS =(
-	"django.contrib.auth.context_processors.auth",
-	"django.core.context_processors.debug",
-	"django.core.context_processors.i18n",
-	"django.core.context_processors.media",
-    "django.core.context_processors.static",
-	'django.contrib.messages.context_processors.messages',
-	"mousedb.context_processors.group_info",
-)
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                "mousedb.context_processors.group_info",
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 INSTALLED_APPS = (
